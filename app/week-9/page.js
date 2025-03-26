9ac18b43e12b996839cd068056fa233d783c13bd
Roma-tech-debug <romanpreetkaur050198@gmail.com>
2025-03-25 20:26:11 -0600

    updates

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -5,31 +5,35 @@ import { useUserAuth } from "./utils/auth-context";
 import Link from "next/link";
 
 export default function Page() {
-    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
- 
-    useEffect(() => {
+  useEffect(() => {
+    if (user) {
+      const authenticateUser = async () => {
         if (!user) {
-            gitHubSignIn(); 
+          await gitHubSignIn();
         }
-    }, [user, gitHubSignIn]); 
+      };
+      authenticateUser();
+    }
+  }, [user, gitHubSignIn]);
 
-    return (
-        <div>
-            {user ? (
-                <>
-                    <p>Welcome, {user.displayName} ({user.email})</p>
-                    <button onClick={firebaseSignOut}>Sign out</button>
+  return (
+    <div>
+      {user ? (
+        <>
+          <p>Welcome, {user.displayName} ({user.email})</p>
+          <button onClick={firebaseSignOut}>Sign out</button>
 
-                    <div>
-                        <Link href="/app/week-9/shopping-list" className="text-orange-200 hover:underline">
-                           Your Shopping List
-                        </Link>
-                    </div>
-                </>
-            ) : (
-                <button onClick={gitHubSignIn}>Sign In GitHub</button>
-            )}
-        </div>
-    );
+          <div>
+            <Link href={"app/week-9/shopping-list"} className="text-orange-200 hover:underline">
+               Your Shopping List
+            </Link>
+          </div>
+        </>
+      ) : (
+        <button onClick={gitHubSignIn}>Sign In with GitHub</button>
+      )}
+    </div>
+  );
 }
